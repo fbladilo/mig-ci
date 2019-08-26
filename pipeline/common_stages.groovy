a147016328646bf09f6ed564c682165a5f937f61
Franco Bladilo <fbladilo@redhat.com>
2019-08-26 16:54:31 -0500

    Only disable docker_storage check

--- pipeline/common_stages.groovy
+++ pipeline/common_stages.groovy
@@ -126,7 +126,7 @@ def deploy_ocp3_agnosticd(kubeconfig) {
             'admin_user': "${cluster_adm_user}",
             // Fix for commit # 8780932 to work
             'course_name': 'ocp-workshop',
-            'openshift_disable_check': 'docker_storage,disk_availability,memory_availability,docker_image_availability',
+            'openshift_disable_check': 'docker_storage',
             'platform': 'aws'
           ]
           sh 'rm -f vars.yml'
